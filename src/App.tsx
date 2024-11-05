2ed20cde3af18d43a9a96416418e9733e672d984
Millie Ellis <millieellis@Millies-MacBook-Pro.local>
2024-11-05 13:50:55 +0900

    Updates to layout to improve look

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import LocalAuthority from './pages/LocalAuthorityPage';
 import './tailwind.css'; 
+import NavBar from './components/NavBar';
 
 const App: React.FC = () => {
     return (
         <div className="App">
+            <NavBar />
             <LocalAuthority />
         </div>
     );
